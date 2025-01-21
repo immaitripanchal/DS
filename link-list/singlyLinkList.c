0c8b304586f13ce827b9fe58573fb416d779dd9b
Maitri Panchal <maitripanchal1408@gmail.com>
2025-01-21 21:18:03 +0530

    initial commit

---
+++ link-list/singlyLinkList.c
@@ -0,0 +1,113 @@
+#include <stdio.h>
+#include <stdlib.h>
+//***************************FUNCTION DECLARATION****************************
+void insert();
+void display();
+void create();
+void deleteAtFirst();
+//***********************NODE***************************
+struct node
+{
+  int data;
+  struct node *next;
+} *head = NULL;
+//******************************MAIN FUNCTION*******************************
+int main()
+{
+  int choice;
+  do
+  {
+    printf("\n-------------------MENU---------------------");
+    printf("\n1.INSERT NODE:");
+    printf("\n2.DISPLAY LINK-LIST :");
+    printf("\n3.CREATE LINK-LIST :");
+    printf("\n4.DELETE AT FIRST :");
+    printf("\n5.DELETER AT LAST :");
+    printf("\n6.DELETE :");
+    printf("\n7.INSERT AT FIRST :");
+    printf("\n8.INSERT BEFORE : ");
+    printf("\n9.INSERT AFTER :");
+    printf("\n10.INSERT AT LAST :");
+    printf("\n11.SORT LINK-LIST :");
+    printf("\n12.REVERSE LINK-LIST :");
+    printf("\n13.SPLIT :");
+    printf("\n14.MERGE :");
+    printf("\n15.EXIT :");
+    printf("\n--------------------------------------------- :");
+
+    printf("\nEnter your choice : ");
+    scanf("%d", &choice);
+
+  } while (choice != 15);
+}
+//*******************************FUNCTION DEFINITION****************************
+//**************************INSERT FUNCTION*******************************
+void insert()
+{
+  struct node *new, *curr;
+  new = (struct node *)malloc(sizeof(struct node));
+  printf("\nEnter the value : ");
+  scanf("%d", new->data);
+  new->next = NULL;
+  if (head == NULL)
+  {
+    head = new;
+  }
+  else
+  {
+    curr = head;
+    while (curr->next != NULL)
+    {
+      curr = curr->next;
+    }
+    curr->next = new;
+  }
+}
+//**********************DISPLAY FUNCTION***********************
+void display()
+{
+  if (head == NULL)
+  {
+    printf("\nList is empty");
+    return;
+  }
+  struct node *curr;
+  curr = head;
+  while (curr->next != NULL)
+  {
+    printf("%d --> ", curr->data);
+    curr = curr->next;
+  }
+}
+//****************************CREATE FUNCTION******************************
+void create()
+{
+  int n;
+  printf("how many nodes you want to create :");
+  scanf("%d", &n);
+  for (int i = 0; i < n; i++)
+  {
+    struct node *new, *curr;
+    new = (struct node *)malloc(sizeof(struct node));
+    printf("\nEnter the value : ");
+    scanf("%d", &new->data);
+    new->next = NULL;
+    if (head == NULL)
+    {
+      head = new;
+    }
+    else
+    {
+      curr = head;
+      while (curr->next != NULL)
+      {
+        curr = curr->next;
+      }
+      curr->next = new;
+    }
+  }
+}
+//**************************DELETE AT FIRST FUNCTION***********************
+void deleteAtFirst()
+{
+}
