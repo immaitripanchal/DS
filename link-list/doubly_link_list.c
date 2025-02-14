02a67ebd56ccb11c6cd064ea4c6fd345e0f0d36b
Maitri Panchal <maitripanchal1408@gmail.com>
2025-02-14 14:36:17 +0530

    intial commit

--- link-list/doubly_link_list.c
+++ link-list/doubly_link_list.c
@@ -1,4 +1,47 @@
 #include <stdio.h>
+#include <stdlib.h>
+//***************************FUNCTION DECLARATION*********************************
+void create();
+void display();
+void delete(int val);
+void insertBefore(int val, int key);
+void insertAfter(int val, int key);
+void insertBeg(int val);
+void insertEnd(int val);
+void reverse();
+void sort();
+//***********************************NODE*****************************************
+struct node
+{
+  struct node *prev;
+  int data;
+  struct node *next;
+} *head = NULL;
+//*********************************MAIN FUNCTION**********************************
 int main()
 {
+  int choice, val;
+  do
+  {
+    printf("\n-----------------MENU-----------------");
+    printf("\n1.CREATE :");
+    printf("\n2.DISPLAY :");
+    printf("\n3.DELETE :");
+    printf("\n4.INSERT BEFORE :");
+    printf("\n5.INSERT AFTER :");
+    printf("\n6.INSERT AT BEGINNING :");
+    printf("\n7.INSERT AT END :");
+    printf("\n8.REVERSE :");
+    printf("\n9.SORTING :");
+    printf("\n10.EXIT :");
+
+    printf("Enter your choice :");
+    scanf("%d", &choice);
+
+    switch (choice)
+    {
+    case 1:
+    }
+  } while (choice != 10);
 }
+//*************************************FUNCTION DEFINITION***********************************
